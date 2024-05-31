daacf8fa9696d2c274b06aa85070cdca4ca0b5bf
tonibauza <tonibauza@alumnat.iesdamiahuguet.net>
2024-05-31 08:39:29 +0200

    Commit volver al index y agregar informacion de todas las secciones hecho

--- consultaApis/scriptLlibres.js
+++ consultaApis/scriptLlibres.js
@@ -32,3 +32,104 @@ fetch(url)
     })
     .catch(err=>console.log(err))}
 
+  async function Personajes(){
+      const url='https://potterapi-fedeperin.vercel.app/es/characters' //cambiar "houses" para diferente info
+
+      fetch(url)
+          .then(response=> response.json() )
+          .then(data => {
+              const table = document.getElementById("alicuotas");
+
+              data.forEach((e,i) => {    //< ---  recorremos data
+
+                  let tr = document.createElement("tr"); //< ---  creamos una fila
+
+                  let td = document.createElement("td"); //< ---  Hacemos columna index dentro de la fila
+                  td.classList.add("index");
+                  td.innerHTML = i;
+                  tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+                  for (p in e) {  //< ---  recorremos cada propiedad de cada elemento
+
+                      let td = document.createElement("td"); //< ---  Hacemos columna dentro de la fila
+                      td.classList.add(p);//<-- le podemos agregar a toda la columna la misma clase
+                      td.innerHTML = e[p];
+
+                      tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+                  }
+
+                  table.appendChild(tr); //< --- Agregamos la fila a la tabla
+
+              });
+
+          })
+          .catch(err=>console.log(err))}
+
+  async function Casas(){
+      const url='https://potterapi-fedeperin.vercel.app/es/houses' //cambiar "houses" para diferente info
+
+      fetch(url)
+          .then(response=> response.json() )
+          .then(data => {
+              const table = document.getElementById("alicuotas");
+
+              data.forEach((e,i) => {    //< ---  recorremos data
+
+                  let tr = document.createElement("tr"); //< ---  creamos una fila
+
+                  let td = document.createElement("td"); //< ---  Hacemos columna index dentro de la fila
+                  td.classList.add("index");
+                  td.innerHTML = i;
+                  tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+                  for (p in e) {  //< ---  recorremos cada propiedad de cada elemento
+
+                      let td = document.createElement("td"); //< ---  Hacemos columna dentro de la fila
+                      td.classList.add(p);//<-- le podemos agregar a toda la columna la misma clase
+                      td.innerHTML = e[p];
+
+                      tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+                  }
+
+                  table.appendChild(tr); //< --- Agregamos la fila a la tabla
+
+              });
+
+          })
+          .catch(err=>console.log(err))}
+
+  async function Hechizos(){
+      const url='https://potterapi-fedeperin.vercel.app/es/spells' //cambiar "houses" para diferente info
+
+      fetch(url)
+          .then(response=> response.json() )
+          .then(data => {
+              const table = document.getElementById("alicuotas");
+
+              data.forEach((e,i) => {    //< ---  recorremos data
+
+                  let tr = document.createElement("tr"); //< ---  creamos una fila
+
+                  let td = document.createElement("td"); //< ---  Hacemos columna index dentro de la fila
+                  td.classList.add("index");
+                  td.innerHTML = i;
+                  tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+                  for (p in e) {  //< ---  recorremos cada propiedad de cada elemento
+
+                      let td = document.createElement("td"); //< ---  Hacemos columna dentro de la fila
+                      td.classList.add(p);//<-- le podemos agregar a toda la columna la misma clase
+                      td.innerHTML = e[p];
+
+                      tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+                  }
+
+                  table.appendChild(tr); //< --- Agregamos la fila a la tabla
+
+              });
+
+          })
+          .catch(err=>console.log(err))}
